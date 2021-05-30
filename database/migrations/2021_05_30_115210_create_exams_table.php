85354bb96ee04c0bb980d71d927c7b3d7f07fe10
Monique Dingding <monique.dingding@lamudi.com>
2021-05-30 20:07:00 +0800

    feat: add new exams table migration

---
+++ database/migrations/2021_05_30_115210_create_exams_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->enum('status', ['open', 'closed']);
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->unsignedInteger('topic_id');
+            $table->foreign('topic_id')->references('id')->on('topics');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index(['deleted_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+}
