452f513450b6373eb125fc8360c3dc6d03ac7cb6
Monique Dingding <monique.dingding@lamudi.com>
2021-05-30 22:04:55 +0800

    chore: fix bug in nav menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,9 @@
+<?php 
+    $validDropdownRoutes = ['users.index', 'roles.index', 'announcements.index', 'users.create', 'roles.create', 'announcements.create', 'users.edit', 'roles.edit', 'announcements.edit'];
+    
+    $isDropdownItemActive = strpos(\Route::current()->getName(), "users") !== false || strpos(\Route::current()->getName(), "roles") !== false || strpos(\Route::current()->getName(), "announcements") !== false;
+?>
+
 <nav class="navbar navbar-height is-fixed-top" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <a class="navbar-item" href="{{ route('home') }}">
@@ -12,38 +18,31 @@
             Home
         </a>
 
-        <a class="navbar-item {{ \Route::current()->getName() === 'topics.index' ? 'navbar-link-is-active' : '' }}"
+        <a class="navbar-item {{ strpos(\Route::current()->getName(), "topics") !== false ? 'navbar-link-is-active' : '' }}"
             href="{{ route('topics.index') }}">
             Courses
         </a>
 
-        <a class="navbar-item {{ \Route::current()->getName() === 'questions.index' ? 'navbar-link-is-active' : '' }}"
+        <a class="navbar-item {{ strpos(\Route::current()->getName(), "exams") !== false ? 'navbar-link-is-active' : '' }}"
             href="{{ route('exams.index') }}">
             Exams
         </a>
 
         <div class="navbar-item has-dropdown is-hoverable">
-            <?php 
-                $validDropdownRoutes = ['users.index', 'roles.index', 'announcements.index', 'users.create', 'roles.create', 'announcements.create', 'users.edit', 'roles.edit', 'announcements.edit'];
-                $isDropdownItemActive = in_array(\Route::current()->getName(), $validDropdownroutes);
-                
-                $isUsersActive = \Route::current()->getName() === 'users.index' || \Route::current()->getName() === 'users.create';
-                $isRolesActive = \Route::current()->getName() === 'roles.index' || \Route::current()->getName() === 'roles.create';
-            ?>
             <a class="navbar-link {{ $isDropdownItemActive ? 'navbar-link-is-active' : '' }}">
             Admin Panel
             </a>
 
             <div class="navbar-dropdown">
-                <a class="navbar-item {{ \Route::current()->getName() === 'announcements.index' ? 'navbar-link-is-active' : '' }}"
+                <a class="navbar-item {{ strpos(\Route::current()->getName(), "announcements") !== false ? 'navbar-link-is-active' : '' }}"
                     href="{{ route('announcements.index') }}">
                     Announcements
                 </a>
-                <a class="navbar-item {{ $isUsersActive ? 'navbar-link-is-active' : '' }}"
+                <a class="navbar-item {{ strpos(\Route::current()->getName(), "users") !== false ? 'navbar-link-is-active' : '' }}"
                     href="{{ route('users.index') }}">
                     Users
                 </a>
-                <a class="navbar-item {{ $isRolesActive ? 'navbar-link-is-active' : '' }}"
+                <a class="navbar-item {{ strpos(\Route::current()->getName(), "roles") !== false ? 'navbar-link-is-active' : '' }}"
                     href="{{ route('roles.index') }}">
                     User Roles
                 </a>
